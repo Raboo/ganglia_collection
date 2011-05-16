dd4919acf7e06fcc6ae8113f2c56116eebbde983
Kevin Mullin <kevin@kpmullin.com>
2011-05-15 17:03:18 -0700

    Almost there, needs testing

--- bind9_stats/bind9_stats-gmetric.rb
+++ bind9_stats/bind9_stats-gmetric.rb
@@ -5,10 +5,22 @@
 require 'rubygems'
 require 'gmetric' # for easy injection into running gmond
 
+$ip = 'localhost' # Ganglia IP/Hostname
+$port = 8649
+
 $rndc_exec = '/usr/sbin/rndc' # rndc location
 $namedstats = '/var/cache/bind/named.stats' # location of new stats file
 $tmp_dir = '/tmp/bindgmetric' # where to store last file
 
+$counter_metrics = {
+    "in_successful_answer" => nil,
+    "caused_recursion" => nil,
+    "non_authoritative_answer" => nil,
+    "resulted_in_nxrrset" => nil,
+    "resulted_in_SERVFAIL" => nil,
+    "resulted_in_NXDOMAIN" => nil,
+    "in_authoritative_answer" => nil
+}
 
 ###############
 #DO NOT EDIT
@@ -25,7 +37,6 @@ if File.directory?($tmp_dir) == false
 end
 
 def reset_stats
-    puts "rndc stats"
     system("echo :> #{$namedstats}; #{$rndc_exec} stats && grep -A 9 'Name Server Statistics' #{$namedstats} | grep -v '++' | grep queries > #{$tmp_stats_file}")
 end
 
@@ -34,13 +45,24 @@ def read_file
     time = 0
     File.open($tmp_stats_file) do |s|
         s.each do |line|
-            value = line.split(' ')[0]
-            metric = line.split(' ')[-3..-1].join('_')
+            value = line.split(' ')[0].to_i
+            metric = line.split(' ')[-3..-1].join('_').gsub('queries_', '')
             hash[metric] = value
         end
     time = s.stat.mtime
     end
     return hash, time
+end
+
+def ganglia_send(metric, value)
+    Ganglia::GMetric.send($ip, $port, {
+        :name => metric,
+        :units => 'queries/sec',
+        :type => 'float',
+        :value => value,
+        :tmax => 60,
+        :dmax => 120
+    })
 end
 
 if File.exist?($tmp_stats_file) == false
@@ -57,5 +79,14 @@ else
     if time_difference < 1
         puts 'needs to be longer than a second'
         exit 1
+    end
+    $counter_metrics.each do |metric, value|
+        rate = (new_data[metric] - old_data[metric]) / time_difference
+        if rate < 0
+            puts 'Somethings wrong. Rate for '+  metric + ' shouldnt be negative.'
+        else
+            puts "#{metric} = #{rate}/sec"
+            ganglia_send(metric, value)
+        end
     end
 end
