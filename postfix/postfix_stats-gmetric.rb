dadf52df2d8455037f4062bfbfeb6b6e1e22b2db
Kevin Mullin <kevin@kpmullin.com>
2011-05-24 17:46:33 -0700

    working on counting postfix

--- postfix/postfix_stats-gmetric.rb
+++ postfix/postfix_stats-gmetric.rb
@@ -3,14 +3,17 @@
 require 'rubygems'
 require 'gmetric'
 
-$ip = 'localhost' # Ganglia IP/Hostname
-$port = 8649
-$metric_group_name = 'bind9'
+POSTFIX_LOG = 'mail.log'
+
+#GMOND
+IP = 'localhost' # Ganglia IP/Hostname
+PORT = 8649
+METRIC_GNAME = 'postfix'
 
 def ganglia_send(metric, value)
-  Ganglia::GMetric.send($ip, $port, {
+  Ganglia::GMetric.send(IP, PORT, {
     :name => metric,
-    :group => $metric_group_name,
+    :group => METRIC_GNAME,
     :units => 'queries/sec',
     :type => 'float',
     :value => value,
@@ -19,4 +22,25 @@ def ganglia_send(metric, value)
   })
 end
 
+def read_log
+  incoming_count, outgoing_count = 0, 0
+  File.open(POSTFIX_LOG) do |s|
+    s.each do |line|
+      case line
+      when /status=sent/
+        case line
+        when /relay=filter/
+          incoming_count += 1
+        when /relay=local/
+        else
+          outgoing_count += 1
+        end
+      end
+    end
+  end
+  puts incoming_count, outgoing_count
+end
 
+
+puts 'starting'
+read_log
