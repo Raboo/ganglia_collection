e04919a9e8876fe09b431f0777fcc5255f80bc36
Kevin Mullin <kevin@kpmullin.com>
2012-03-22 21:52:56 -0700

    updated bind9 script to be more resilient

--- bind9/bind9_stats-gmetric.rb
+++ bind9/bind9_stats-gmetric.rb
@@ -5,94 +5,99 @@
 require 'rubygems'
 require 'gmetric' # for easy injection into running gmond
 
-$ip = 'localhost' # Ganglia IP/Hostname
-$port = 8649
-$metric_group_name = 'bind9'
+IP = 'localhost' # Ganglia IP/Hostname
+PORT = 8649
+METRIC_GROUP_NAME = 'bind9'
 
-$rndc_exec = '/usr/sbin/rndc' # rndc location
-$namedstats = '/var/cache/bind/named.stats' # location of new stats file
-$tmp_dir = '/tmp/bindgmetric' # where to store last file
+DEBUG = true
+NAMEDSTATS = '/var/cache/bind/named.stats' # location of new stats file
+TMP_DIR = '/tmp/bindgmetric' # where to store last file
 
-$counter_metrics = {
-  "in_successful_answer" => nil,
-  "caused_recursion" => nil,
-  "non_authoritative_answer" => nil,
-  "resulted_in_nxrrset" => nil,
-  "resulted_in_SERVFAIL" => nil,
-  "resulted_in_NXDOMAIN" => nil,
-  "in_authoritative_answer" => nil
-}
-
-###############
-#DO NOT EDIT
-#
-if File.executable_real?($rndc_exec) == false
-  puts "Cannot exec #{$rndc_exec}"
+RNDC_EXEC = `which rndc`.chomp # rndc location
+unless File.executable_real?(RNDC_EXEC)
+  puts "Cannot exec #{RNDC_EXEC}"
   exit 1
 end
 
-$tmp_stats_file = "#{$tmp_dir}/bindstats"
-if File.directory?($tmp_dir) == false
+unless File.directory?(TMP_DIR)
   puts 'no tmp dir; creating'
-  system("mkdir -p #{$tmp_dir}")
+  system("mkdir -p #{TMP_DIR}")
 end
 
-def reset_stats
-  system(":> #{$namedstats}; #{$rndc_exec} stats && grep -A 9 'Name Server Statistics' #{$namedstats} | grep -v '++' | grep queries > #{$tmp_stats_file}")
-end
+class BindGMetric
+  def initialize
+    @tmp_stats_file = "#{TMP_DIR}/bindstats"
+    @counter_metrics = {
+      "in_successful_answer" => nil,
+      "caused_recursion" => nil,
+      "non_authoritative_answer" => nil,
+      "resulted_in_nxrrset" => nil,
+      "resulted_in_SERVFAIL" => nil,
+      "resulted_in_NXDOMAIN" => nil,
+      "in_authoritative_answer" => nil
+    }
+  end
 
-def read_file
-  hash = {}
-  time = 0
-  File.open($tmp_stats_file) do |s|
-    s.each do |line|
-      value = line.split(' ')[0].to_i
-      metric = line.split(' ')[-3..-1].join('_').gsub('queries_', '')
+  def reset_stats
+    system(":> #{NAMEDSTATS}; #{RNDC_EXEC} stats && grep -A 16 'Name Server Statistics' #{NAMEDSTATS} | grep -v '++' | grep queries > #{@tmp_stats_file}")
+  end
+
+  def read_file
+    hash = {}
+    f = File.open(@tmp_stats_file)
+    f.each do |line|
+      value = line.split[0].to_i
+      metric = line.split[-3..-1].delete_if { |x| x =~ /\d/ }.join('_').gsub('queries_', '')
       hash[metric] = value
     end
-    time = s.stat.mtime
+    return hash, f.stat.mtime
   end
-  return hash, time
-end
 
-def ganglia_send(metric, value)
-  Ganglia::GMetric.send($ip, $port, {
-    :name => metric,
-    :group => $metric_group_name,
-    :units => 'queries/sec',
-    :type => 'float',
-    :value => value,
-    :tmax => 60,
-    :dmax => 120
-  })
-end
-
-if File.exist?($namedstats) == false
-  system("#{rndc_exec} stats")
-end
-
-if File.exist?($tmp_stats_file) == false
-  # no previous data, clean stats file and re stat
-  puts 'Creating baseline, no data reported'
-  reset_stats
-else
-  # open old file and read data to get baseline
-  old_data, old_time = read_file
-  # we read old data, now need new one
-  reset_stats
-  new_data, new_time = read_file
-  time_difference = new_time - old_time
-  if time_difference < 1
-    puts 'needs to be longer than a second'
-    exit 1
+  def ganglia_send(metric, value)
+    Ganglia::GMetric.send(IP, PORT, {
+      :name => metric,
+      :group => METRIC_GROUP_NAME,
+      :units => 'queries/sec',
+      :type => 'float',
+      :value => value,
+      :tmax => 60,
+      :dmax => 120
+    }) unless DEBUG
+    log "#{metric} = #{value}/sec"
   end
-  $counter_metrics.each do |metric, value|
-    rate = (new_data[metric] - old_data[metric]) / time_difference
-    if rate < 0
-      puts 'Somethings wrong. Rate for ' +  metric + ' shouldnt be negative.'
+
+  def run
+    unless File.exist?(@tmp_stats_file)
+      # no previous data, clean stats file and re stat
+      log 'Creating baseline, no data reported'
+      reset_stats
     else
-      puts "#{metric} = #{rate}/sec"
-      ganglia_send(metric, rate)
+      # open old file and read data to get baseline
+      old_data, old_time = read_file
+      # we read old data, now need new one
+      reset_stats
+      new_data, new_time = read_file
+      time_difference = new_time - old_time
+      if time_difference < 1
+        log 'needs to be longer than a second'
+        return 1
+      end
+      old_data.each_key do |metric|
+        rate = (new_data[metric] - old_data[metric]) / time_difference
+        if rate < 0
+          log 'Somethings wrong. Rate for ' +  metric + ' shouldnt be negative.'
+        else
+          ganglia_send(metric, rate) if @counter_metrics.keys.include?(metric)
+        end
+      end
     end
   end
+
+  def log(msg)
+    puts msg if DEBUG
+  end
+
 end
+
+a = BindGMetric.new
+a.run
